ab4f1d0e293922dc170a7fa46dbf54491b484d89
sergelenks <45604260+sergelenks@users.noreply.github.com>
2025-01-30 03:24:38 +0000

    movie struct and create movie

--- movies.c
+++ movies.c
@@ -9,3 +9,29 @@ struct movie {
     double rating;
     struct movie* next;
 };
+
+struct movie* createMovie(char* title, int year, char* languages, double rating) {
+    struct movie* movie = malloc(sizeof(struct movie));
+    
+    movie->title = malloc(strlen(title) + 1);
+    strcpy(movie->title, title);
+    
+    movie->languages = malloc(strlen(languages) + 1);
+    strcpy(nmovie->languages, languages);
+    
+    movie->year = year;
+    movie->rating = rating;
+    movie->next = NULL;
+    return movie;
+}
+
+int main (int argc, char **argv){
+    if (argc < 2)
+    {
+        printf("You must provide the name of the file to process\n");
+        printf("Example usage: ./movies movies.csv\n");
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
